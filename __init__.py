7605315096794b024a2eae2f6f756524ab2fbc26
Tab Atkins <jackalmage@gmail.com>
2017-08-24 16:36:03 -0700

    update data

--- __init__.py
+++ __init__.py
@@ -5,12 +5,17 @@ from __future__ import division, unicode_literals
 
 import bikeshed
 import os
+import subprocess
 
 def main():
-	path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
+	scriptPath = os.path.dirname(os.path.realpath(__file__))
+	dataPath = os.path.join(scriptPath, "data")
 	bikeshed.config.quiet = False
-	bikeshed.update.update(path=path)
-	bikeshed.update.createManifest(path=path)
+	#bikeshed.update.update(path=dataPath)
+	#bikeshed.update.createManifest(path=dataPath)
+	os.chdir(scriptPath)
+	print subprocess.check_output("git add .", shell=True)
+	print subprocess.check_output("git push", shell=True)
 
 if __name__ == "__main__":
     main()
