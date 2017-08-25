66ce8b3a86d076c500ecd0c15fd83c53878c37ab
Tab Atkins <jackalmage@gmail.com>
2017-08-25 13:11:58 -0700

    Don't commit if the manifest is identical.

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@
 from __future__ import division, unicode_literals
 
 import bikeshed
+import io
 import os
 import subprocess
 
@@ -12,7 +13,20 @@ def main():
 	dataPath = os.path.join(scriptPath, "data")
 	bikeshed.config.quiet = False
 	bikeshed.update.update(path=dataPath)
+	try:
+		with io.open(os.path.join(dataPath, "manifest.txt"), 'r', encoding="utf-8") as fh:
+			_,_,oldManifest = fh.read().partition("\n")
+	except:
+		raise
 	bikeshed.update.createManifest(path=dataPath)
+	try:
+		with io.open(os.path.join(dataPath, "manifest.txt"), 'r', encoding="utf-8") as fh:
+			_,_,newManifest = fh.read().partition("\n")
+	except:
+		raise
+	if oldManifest == newManifest:
+		# No change
+		return
 	os.chdir(scriptPath)
 	subprocess.check_call("git add data", shell=True)
 	subprocess.check_call("git commit -m 'update data'", shell=True)
