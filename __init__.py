457d1423c453359a85a56ee2c75533f5fee46cbe
Tab Atkins-Bittner <jackalmage@gmail.com>
2018-09-20 14:48:07 -0700

    Remove the old WPT stuff.

--- __init__.py
+++ __init__.py
@@ -43,46 +43,10 @@ def updateAndCommit():
 	print subprocess.check_output("git push", shell=True)
 
 def updateDataFiles(path):
-	# First, update the Bikeshed-managed data files
-	# (This updates the manifest file automatically.)
 	bikeshed.config.quiet = False
 	bikeshed.update.update(path=path, force=True)
 
-	# Then the WPT data files, adding the file to Bikeshed's data folder
-	#updateWptDataFiles(dataPath=path)
 
-	# And now manually update the Bikeshed manifest again.
-	#bikeshed.update.manifest.createManifest(path=path)
-
-def updateWptDataFiles(dataPath):
-	return # no-op for now until WPT is fixed
-	scriptPath = os.path.dirname(os.path.realpath(__file__))
-	wptPath = os.path.join(scriptPath, "web-platform-tests")
-
-	os.chdir(scriptPath)
-	try:
-		subprocess.check_call("git submodule update --remote", shell=True)
-	except Exception as e:
-		print e
-		return
-	os.chdir(wptPath)
-	try:
-		subprocess.check_call("wpt manifest", shell=True)
-	except Exception as e:
-		print e
-		return
-
-	paths = []
-	with io.open(os.path.join(wptPath, "MANIFEST.json"), 'r', encoding="utf-8") as infile:
-		jsonData = json.load(infile)
-		for testType, typePaths in jsonData["items"].items():
-			if testType in ("support", "reftest_node"):
-				# Not tests
-				continue
-			paths.extend(typePaths.keys())
-	with io.open(os.path.join(dataPath, "wpt-tests.txt"), "w", encoding="utf-8") as outfile:
-		for path in sorted(paths):
-			outfile.write(path + "\n")
 
 if __name__ == "__main__":
 	main()
